129c40fa1fa0f2140042223cc61cf5bf4ed9b746
Rabten Samphel <rabten@protonmail.com>
2022-01-03 01:37:36 -0500

    completed dot navigation 🏋️‍♀️

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const leftArrow = document.getElementById('left')
 let cur = 0
 let max = imgs.length
 
+
+function styleDot() {
+    const dots = document.querySelectorAll('.dot')
+    let newDots = Array.from(dots)
+    let curDot = dots[cur]
+    curDot.style.backgroundColor = '#fff'
+
+    let rest = newDots.filter(el => el !== curDot)
+
+    rest.forEach(el => {
+        el.style.backgroundColor = '#000'
+    })
+}
+
+styleDot()
+
 imgs.forEach((el, i) => {
     el.style.transform = `translateX(${100 * (i - cur)}%)`
 })
@@ -21,6 +37,7 @@ rightArrow.addEventListener('click', () => {
     imgs.forEach((el, i) => {
         el.style.transform = `translateX(${100 * (i - cur)}%)`
     })
+    styleDot()
 })
 
 
@@ -34,4 +51,5 @@ leftArrow.addEventListener('click', () => {
     imgs.forEach((el, i) => {
         el.style.transform = `translateX(${100 * (i - cur)}%)`
     })
+    styleDot()
 })
